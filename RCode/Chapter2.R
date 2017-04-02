f0840d19064379859a49ef39d09de6db0f267669
Muqadder Iqbal <dj.laaal@gmail.com>
2017-04-02 16:42:02 -0700

    First commit to master.

---
+++ RCode/Chapter2.R
@@ -0,0 +1,9 @@
+x <- c(1,4,6)
+y <- c(2,3,5)
+x + y
+length(x)
+length(y)
+rm(list = ls())
+x= matrix ( data=c(1 ,2 ,3 ,4) , nrow =2, ncol =2)
+x
+matrix ( data=c(1 ,2 ,3 ,4) , nrow =2, ncol =2 , byrow = TRUE)
